fb074d892da48d1036ae8fde57ed326d0388d5b0
AkhtarWasim <104205497+AkhtarWasim@users.noreply.github.com>
2022-04-28 17:21:00 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'sampleProject';
+
+  constructor(private router: Router) {
+
+
+  }
+
+  ngOnInit(): void { }
+  getDataByTranId(event: any) {
+    let tranId = event.target.value;
+    if (!!tranId) {
+      this.router.navigate(['/home/reports'], { queryParams: { ID: tranId } });
+    }
+  }
+}
