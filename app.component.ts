2d0e97f5dbd0d0eecbd23a6badac8a404430b59e
AkhtarWasim <104205497+AkhtarWasim@users.noreply.github.com>
2022-05-12 22:27:36 +0530

    Update app.component.ts

--- app.component.ts
+++ app.component.ts
@@ -8,6 +8,7 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'sampleProject';
+  isActive = false;
 
   constructor(private router: Router) {
 
@@ -20,5 +21,15 @@ export class AppComponent {
     if (!!tranId) {
       this.router.navigate(['/home/reports'], { queryParams: { ID: tranId } });
     }
+  }
+
+  isActiveChange(event: any) {
+    console.log(event.target.checked);
+    let tranId = event.target.value;
+
+    if (event.target.checked) {
+      this.isActive = true;
+    }
+    else { this.isActive = false;}
   }
 }
